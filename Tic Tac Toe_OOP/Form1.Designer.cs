81e159f3bae9c1d8d82b8166cf11722011b413e5
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2021-11-17 17:47:46 +0800

    Add project files.

---
+++ Tic Tac Toe_OOP/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Tic_Tac_Toe_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTic1 = new System.Windows.Forms.Button();
+            this.btnTic2 = new System.Windows.Forms.Button();
+            this.btnTic3 = new System.Windows.Forms.Button();
+            this.btnTic4 = new System.Windows.Forms.Button();
+            this.btnTic5 = new System.Windows.Forms.Button();
+            this.btnTic6 = new System.Windows.Forms.Button();
+            this.btnTic7 = new System.Windows.Forms.Button();
+            this.btnTic8 = new System.Windows.Forms.Button();
+            this.btnTic9 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTic1
+            // 
+            this.btnTic1.BackColor = System.Drawing.Color.Snow;
+            this.btnTic1.Location = new System.Drawing.Point(12, 76);
+            this.btnTic1.Name = "btnTic1";
+            this.btnTic1.Size = new System.Drawing.Size(86, 82);
+            this.btnTic1.TabIndex = 0;
+            this.btnTic1.UseVisualStyleBackColor = false;
+            this.btnTic1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTic2
+            // 
+            this.btnTic2.BackColor = System.Drawing.Color.Snow;
+            this.btnTic2.Location = new System.Drawing.Point(104, 76);
+            this.btnTic2.Name = "btnTic2";
+            this.btnTic2.Size = new System.Drawing.Size(86, 82);
+            this.btnTic2.TabIndex = 1;
+            this.btnTic2.UseVisualStyleBackColor = false;
+            this.btnTic2.Click += new System.EventHandler(this.btnTic2_Click);
+            // 
+            // btnTic3
+            // 
+            this.btnTic3.BackColor = System.Drawing.Color.Snow;
+            this.btnTic3.Location = new System.Drawing.Point(196, 76);
+            this.btnTic3.Name = "btnTic3";
+            this.btnTic3.Size = new System.Drawing.Size(86, 82);
+            this.btnTic3.TabIndex = 2;
+            this.btnTic3.UseVisualStyleBackColor = false;
+            this.btnTic3.Click += new System.EventHandler(this.btnTic3_Click);
+            // 
+            // btnTic4
+            // 
+            this.btnTic4.BackColor = System.Drawing.Color.Snow;
+            this.btnTic4.Location = new System.Drawing.Point(12, 164);
+            this.btnTic4.Name = "btnTic4";
+            this.btnTic4.Size = new System.Drawing.Size(86, 82);
+            this.btnTic4.TabIndex = 3;
+            this.btnTic4.UseVisualStyleBackColor = false;
+            this.btnTic4.Click += new System.EventHandler(this.btnTic4_Click);
+            // 
+            // btnTic5
+            // 
+            this.btnTic5.BackColor = System.Drawing.Color.Snow;
+            this.btnTic5.Location = new System.Drawing.Point(104, 164);
+            this.btnTic5.Name = "btnTic5";
+            this.btnTic5.Size = new System.Drawing.Size(86, 82);
+            this.btnTic5.TabIndex = 4;
+            this.btnTic5.UseVisualStyleBackColor = false;
+            this.btnTic5.Click += new System.EventHandler(this.btnTic5_Click);
+            // 
+            // btnTic6
+            // 
+            this.btnTic6.BackColor = System.Drawing.Color.Snow;
+            this.btnTic6.Location = new System.Drawing.Point(196, 164);
+            this.btnTic6.Name = "btnTic6";
+            this.btnTic6.Size = new System.Drawing.Size(86, 82);
+            this.btnTic6.TabIndex = 5;
+            this.btnTic6.UseVisualStyleBackColor = false;
+            this.btnTic6.Click += new System.EventHandler(this.btnTic6_Click);
+            // 
+            // btnTic7
+            // 
+            this.btnTic7.BackColor = System.Drawing.Color.Snow;
+            this.btnTic7.Location = new System.Drawing.Point(12, 252);
+            this.btnTic7.Name = "btnTic7";
+            this.btnTic7.Size = new System.Drawing.Size(86, 82);
+            this.btnTic7.TabIndex = 6;
+            this.btnTic7.UseVisualStyleBackColor = false;
+            this.btnTic7.Click += new System.EventHandler(this.btnTic7_Click);
+            // 
+            // btnTic8
+            // 
+            this.btnTic8.BackColor = System.Drawing.Color.Snow;
+            this.btnTic8.Location = new System.Drawing.Point(104, 252);
+            this.btnTic8.Name = "btnTic8";
+            this.btnTic8.Size = new System.Drawing.Size(86, 82);
+            this.btnTic8.TabIndex = 7;
+            this.btnTic8.UseVisualStyleBackColor = false;
+            this.btnTic8.Click += new System.EventHandler(this.btnTic8_Click);
+            // 
+            // btnTic9
+            // 
+            this.btnTic9.BackColor = System.Drawing.Color.Snow;
+            this.btnTic9.Location = new System.Drawing.Point(196, 252);
+            this.btnTic9.Name = "btnTic9";
+            this.btnTic9.Size = new System.Drawing.Size(86, 82);
+            this.btnTic9.TabIndex = 8;
+            this.btnTic9.UseVisualStyleBackColor = false;
+            this.btnTic9.Click += new System.EventHandler(this.btnTic9_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Snow;
+            this.btnReset.Location = new System.Drawing.Point(196, 340);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(86, 30);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = global::Tic_Tac_Toe_OOP.Properties.Resources._256039336_676785103291692_8251907035004046222_n;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(295, 382);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnTic9);
+            this.Controls.Add(this.btnTic8);
+            this.Controls.Add(this.btnTic7);
+            this.Controls.Add(this.btnTic6);
+            this.Controls.Add(this.btnTic5);
+            this.Controls.Add(this.btnTic4);
+            this.Controls.Add(this.btnTic3);
+            this.Controls.Add(this.btnTic2);
+            this.Controls.Add(this.btnTic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTic1;
+        private System.Windows.Forms.Button btnTic2;
+        private System.Windows.Forms.Button btnTic3;
+        private System.Windows.Forms.Button btnTic4;
+        private System.Windows.Forms.Button btnTic5;
+        private System.Windows.Forms.Button btnTic6;
+        private System.Windows.Forms.Button btnTic7;
+        private System.Windows.Forms.Button btnTic8;
+        private System.Windows.Forms.Button btnTic9;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Tic Tac Toe_OOP/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_OOP
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        bool isXorO;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic1.Text = "X";
+                btnTic1.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic1.Text = "O";
+                btnTic1.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic1.Text = "X";
+                btnTic1.Enabled = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTic3_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic3.Text = "X";
+                btnTic3.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic3.Text = "O";
+                btnTic1.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic3.Text = "X";
+                btnTic3.Enabled = false;
+            }
+        }
+
+        private void btnTic2_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic2.Text = "X";
+                btnTic2.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic2.Text = "O";
+                btnTic2.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic2.Text = "X";
+                btnTic2.Enabled = false;
+            }
+        }
+
+        private void btnTic4_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic4.Text = "X";
+                btnTic4.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic4.Text = "O";
+                btnTic4.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic4.Text = "X";
+                btnTic4.Enabled = false;
+            }
+        }
+
+        private void btnTic5_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic5.Text = "X";
+                btnTic5.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic5.Text = "O";
+                btnTic5.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic5.Text = "X";
+                btnTic5.Enabled = false;
+            }
+        }
+
+        private void btnTic6_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic6.Text = "X";
+                btnTic6.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic6.Text = "O";
+                btnTic6.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic6.Text = "X";
+                btnTic6.Enabled = false;
+            }
+        }
+
+        private void btnTic7_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic7.Text = "X";
+                btnTic7.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic7.Text = "O";
+                btnTic7.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic7.Text = "X";
+                btnTic7.Enabled = false;
+            }
+        }
+
+        private void btnTic8_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic8.Text = "X";
+                btnTic8.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic8.Text = "O";
+                btnTic8.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic8.Text = "X";
+                btnTic8.Enabled = false;
+            }
+        }
+
+        private void btnTic9_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                count += 1;
+                btnTic9.Text = "X";
+                btnTic9.Enabled = false;
+                isXorO = false;
+            }
+            else if (!isXorO)
+            {
+                isXorO = true;
+                count += 1;
+                btnTic9.Text = "O";
+                btnTic9.Enabled = false;
+            }
+            else
+            {
+                count += 1;
+                isXorO = false;
+                btnTic9.Text = "X";
+                btnTic9.Enabled = false;
+            }
+        }
+    }
+}
